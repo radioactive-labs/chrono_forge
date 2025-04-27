058b2be7641585a696846bb5d916aa9020fd5776
TheDumbTechGuy <sfroelich01@gmail.com>
2025-04-27 14:26:34 +0000

    fix name

--- lib/chrono_forge.rb
+++ lib/chrono_forge.rb
@@ -4,7 +4,7 @@ require "zeitwerk"
 require "active_record"
 require "active_job"
 
-module Chronoforge
+module ChronoForge
   Loader = Zeitwerk::Loader.for_gem.tap do |loader|
     loader.ignore("#{__dir__}/generators")
     loader.setup
